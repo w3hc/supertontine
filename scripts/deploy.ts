2d126dddbfcd392c4c8f192e4196abe1a82954df
Julien Béranger <julien@strat.cc>
2023-09-10 09:54:04 +0200

    Add `tontine.config.ts` file (#4)

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -2,9 +2,15 @@ const color = require("cli-color")
 var msg = color.xterm(39).bgXterm(128)
 import hre, { ethers, network } from 'hardhat'
 import fs from 'fs'
+import * as tontine from '../tontine.config'
 
 async function main() {
 
+  console.log("Duration:", tontine.roundDuration)
+  console.log("First members:", tontine.firstMembers)
+  console.log("Amount:", tontine.monthlyContribAmount)
+
+  // deploy
   const initialMint = ethers.parseEther("10000")
   const USD = await ethers.getContractFactory("USD")
   const usd = await USD.deploy(initialMint)
@@ -13,11 +19,17 @@ async function main() {
     "contractAddress": await usd.getAddress()
   }
   
+  // write the contract address in store.json
   const content = JSON.stringify(recordAddress, null, 2)
   fs.writeFileSync('store.json', content)
 
-  console.log('Basic ERC-20 token contract deployed:', msg(await usd.getAddress()))
+  console.log('\nUSD contract deployed:', msg(await usd.getAddress()))
 
+  /*
+  *
+  * Uncomment the part below to activate the Etherscan verification.
+  * 
+  */
   // try {
   //   console.log("\nEtherscan verification in progress...")
   //   console.log("\nWaiting for 6 block confirmations (you can skip this part)")
