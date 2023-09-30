4bb89da4fd7e8e1f4e2b61329ef4b63d71c296c9
julienbrg <julien@strat.cc>
2023-09-30 12:11:37 +0200

    add setReceiver

--- contracts/TontineLogic.sol
+++ contracts/TontineLogic.sol
@@ -12,25 +12,34 @@ import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
  *
  */
 contract TontineLogic is RedirectAll {
-
     uint256 public roundDuration;
     uint256 public monthlyContribAmount;
     uint256 public currentPeriod;
     uint256 public currentID;
     address public membershipNFTAddress;
-    
+
     // host on Goerli maybe 0x3E14dC1b13c488a8d5D310918780c983bD5982E7
-    constructor(uint256 _roundDuration, uint256 _monthlyContribAmount, ISuperToken _acceptedToken, ISuperfluid _host, address _owner, address _membershipNFTAddress) RedirectAll(_acceptedToken, host, _owner) {
-        roundDuration=_roundDuration;
-        monthlyContribAmount=_monthlyContribAmount;
+    constructor(
+        uint256 _roundDuration,
+        uint256 _monthlyContribAmount,
+        ISuperToken _acceptedToken,
+        ISuperfluid _host,
+        address _owner,
+        address _membershipNFTAddress
+    ) RedirectAll(_acceptedToken, host, _owner) {
+        roundDuration = _roundDuration;
+        monthlyContribAmount = _monthlyContribAmount;
         currentPeriod = block.timestamp;
         currentID = 0;
-        membershipNFTAddress= _membershipNFTAddress;
+        membershipNFTAddress = _membershipNFTAddress;
     }
 
-    function switch() public {
-        require((currentPeriod + 1 month) < block.timestamp, "Too soon to switch")
-        address to = ERC721(membershipNFTAddress).ownerOf(currentID+1)
+    function setReceiver() public {
+        require(
+            (currentPeriod + 4 weeks) < block.timestamp,
+            "Too soon to switch"
+        );
+        address to = ERC721(membershipNFTAddress).ownerOf(currentID + 1);
         _changeReceiver(to);
         currentID++;
     }
